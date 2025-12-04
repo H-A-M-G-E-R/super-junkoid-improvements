248000320127d90aab5f30ca78dca9826f5b0289
H-A-M-G-E-R <hamgerham@proton.me>
2025-12-03 23:41:44 -0600

    optimize profane junko spritemaps and gfx

---
+++ spritemaps/ProfaneJunko.asm
@@ -0,0 +1,254 @@
+ProfaneJunkoGfx:
+incbin "ProfaneJunko.gfx"
+
+ProfaneJunkoPal:
+incbin "ProfaneJunko.pal"
+
+ProfaneJunkoSpritemap_0_A6EC5B:
+dw $0006 : db $F4,$81,$02,$2E,$31, $EC,$81,$02,$2D,$31, $FC,$81,$F2,$2B,$31, $EC,$81,$F2,$29,$31, $FC,$01,$EA,$03,$31, $F4,$01,$EA,$02,$31
+
+ProfaneJunkoSpritemap_1_A6EC99:
+dw $0006 : db $06,$00,$F8,$56,$31, $FE,$81,$00,$49,$31, $F6,$81,$F0,$44,$31, $EE,$81,$00,$47,$31, $E6,$81,$F0,$42,$31, $F6,$81,$E0,$40,$31
+
+ProfaneJunkoSpritemap_2_A6ECDC:
+dw $0007 : db $00,$00,$DD,$5B,$31, $F8,$01,$05,$77,$31, $00,$80,$F5,$65,$31, $00,$80,$E5,$62,$31, $F0,$81,$E5,$60,$31, $F8,$01,$DD,$4B,$31, $F8,$81,$F5,$64,$31
+
+ProfaneJunkoSpritemap_3_A6ED29:
+dw $0002 : db $FA,$81,$FF,$4E,$31, $EA,$81,$FF,$4C,$31
+
+ProfaneJunkoSpritemap_4_A6ED4E:
+dw $0002 : db $F9,$81,$0B,$4E,$31, $E9,$81,$0B,$4C,$31
+
+ProfaneJunkoSpritemap_5_A6ED6E:
+dw $0002 : db $FD,$81,$13,$4E,$31, $ED,$81,$13,$4C,$31
+
+ProfaneJunkoSpritemap_6_A6ED8E:
+dw $0000
+
+ProfaneJunkoSpritemap_7_A6ED95:
+dw $0005 : db $F0,$81,$F8,$20,$31, $00,$80,$F8,$22,$31, $FE,$81,$06,$22,$31, $00,$00,$F0,$12,$31, $F0,$81,$E8,$00,$31
+
+ProfaneJunkoSpritemap_8_A6EDB5:
+dw $0008 : db $00,$80,$10,$6E,$71, $00,$80,$00,$6C,$71, $00,$80,$F0,$6A,$71, $00,$80,$E0,$68,$71, $F0,$81,$10,$6E,$31, $F0,$81,$00,$6C,$31, $F0,$81,$F0,$6A,$31, $F0,$81,$E0,$68,$31
+
+ProfaneJunkoSpritemap_9_A6EE57:
+dw $0006 : db $FC,$81,$02,$2E,$71, $04,$80,$02,$2D,$71, $F4,$81,$F2,$2B,$71, $04,$80,$F2,$29,$71, $FC,$01,$EA,$03,$71, $04,$00,$EA,$02,$71
+
+ProfaneJunkoSpritemap_10_A6EE95:
+dw $0006 : db $F2,$01,$F8,$56,$71, $F2,$81,$00,$49,$71, $FA,$81,$F0,$44,$71, $02,$80,$00,$47,$71, $0A,$80,$F0,$42,$71, $FA,$81,$E0,$40,$71
+
+ProfaneJunkoSpritemap_11_A6EED8:
+dw $0007 : db $F8,$01,$DD,$5B,$71, $00,$00,$05,$77,$71, $F0,$81,$F5,$65,$71, $F0,$81,$E5,$62,$71, $00,$80,$E5,$60,$71, $00,$00,$DD,$4B,$71, $F8,$81,$F5,$64,$71
+
+ProfaneJunkoSpritemap_12_A6EF25:
+dw $0002 : db $F6,$81,$FF,$4E,$71, $06,$80,$FF,$4C,$71
+
+ProfaneJunkoSpritemap_13_A6EF4A:
+dw $0002 : db $F7,$81,$0B,$4E,$71, $07,$80,$0B,$4C,$71
+
+ProfaneJunkoSpritemap_14_A6EF6A:
+dw $0002 : db $F3,$81,$13,$4E,$71, $03,$80,$13,$4C,$71
+
+ProfaneJunkoSpritemap_15_A6EF8A:
+dw $0000
+
+ProfaneJunkoSpritemap_16_A6EF91:
+dw $0005 : db $00,$80,$F8,$20,$71, $F0,$81,$F8,$22,$71, $F2,$81,$06,$22,$71, $F8,$01,$F0,$12,$71, $00,$80,$E8,$00,$71
+
+ProfaneJunkoTailSpritemap_0_A6DC90:
+dw $0001 : db $F8,$81,$F8,$80,$31
+
+ProfaneJunkoTailSpritemap_1_A6DC97:
+dw $0001 : db $F8,$81,$F8,$82,$31
+
+ProfaneJunkoTailSpritemap_2_A6DC9E:
+dw $0001 : db $F8,$81,$F8,$84,$31
+
+ProfaneJunkoTailSpritemap_3_A6DCA5:
+dw $0001 : db $F8,$81,$F8,$80,$71
+
+ProfaneJunkoTailSpritemap_4_A6DCAC:
+dw $0001 : db $F8,$81,$F8,$82,$71
+
+ProfaneJunkoTailSpritemap_5_A6DCB3:
+dw $0001 : db $F8,$81,$F8,$84,$71
+
+ProfaneJunkoTailTipWingSpritemap_0_A6DCDA:
+dw $0001 : db $F0,$81,$F8,$86,$31
+
+ProfaneJunkoTailTipWingSpritemap_1_A6DCE1:
+dw $0001 : db $F0,$81,$F4,$88,$31
+
+ProfaneJunkoTailTipWingSpritemap_2_A6DCE8:
+dw $0001 : db $F2,$81,$F3,$8A,$31
+
+ProfaneJunkoTailTipWingSpritemap_3_A6DCEF:
+dw $0001 : db $F4,$81,$F0,$8C,$31
+
+ProfaneJunkoTailTipWingSpritemap_4_A6DCF6:
+dw $0001 : db $F8,$81,$F0,$8E,$31
+
+ProfaneJunkoTailTipWingSpritemap_5_A6DCFD:
+dw $0001 : db $FC,$81,$F0,$8C,$71
+
+ProfaneJunkoTailTipWingSpritemap_6_A6DD04:
+dw $0001 : db $FE,$81,$F3,$8A,$71
+
+ProfaneJunkoTailTipWingSpritemap_7_A6DD0B:
+dw $0001 : db $00,$80,$F4,$88,$71
+
+ProfaneJunkoTailTipWingSpritemap_8_A6DD12:
+dw $0001 : db $00,$80,$F8,$86,$71
+
+ProfaneJunkoTailTipWingSpritemap_9_A6DD19:
+dw $0001 : db $00,$80,$FC,$88,$F1
+
+ProfaneJunkoTailTipWingSpritemap_10_A6DD20:
+dw $0001 : db $FE,$81,$FE,$8A,$F1
+
+ProfaneJunkoTailTipWingSpritemap_11_A6DD27:
+dw $0001 : db $FC,$81,$00,$8C,$F1
+
+ProfaneJunkoTailTipWingSpritemap_12_A6DD2E:
+dw $0001 : db $F9,$81,$00,$8E,$F1
+
+ProfaneJunkoTailTipWingSpritemap_13_A6DD35:
+dw $0001 : db $F4,$81,$FF,$8C,$B1
+
+ProfaneJunkoTailTipWingSpritemap_14_A6DD3C:
+dw $0001 : db $F2,$81,$FE,$8A,$B1
+
+ProfaneJunkoTailTipWingSpritemap_15_A6DD43:
+dw $0001 : db $F0,$81,$FC,$88,$B1
+
+ProfaneJunkoTailTipWingSpritemap_16_A6DD4A:
+dw $0002 : db $14,$80,$DF,$05,$31, $0C,$80,$DF,$04,$31
+
+ProfaneJunkoTailTipWingSpritemap_17_A6DD6A:
+dw $0002 : db $14,$80,$E4,$08,$31, $0C,$80,$E4,$07,$31
+
+ProfaneJunkoTailTipWingSpritemap_18_A6DD85:
+dw $0002 : db $1C,$00,$F0,$36,$31, $0C,$80,$E8,$24,$31
+
+ProfaneJunkoTailTipWingSpritemap_19_A6DD96:
+dw $0002 : db $1C,$00,$E8,$36,$B1, $0C,$80,$E8,$24,$B1
+
+ProfaneJunkoTailTipWingSpritemap_20_A6DDA7:
+dw $0002 : db $14,$80,$EA,$08,$B1, $0C,$80,$EA,$07,$B1
+
+ProfaneJunkoTailTipWingSpritemap_21_A6DDC2:
+dw $0002 : db $14,$80,$EF,$05,$B1, $0C,$80,$EF,$04,$B1
+
+ProfaneJunkoTailTipWingSpritemap_22_A6DDE2:
+dw $0002 : db $DC,$81,$DF,$05,$71, $E4,$81,$DF,$04,$71
+
+ProfaneJunkoTailTipWingSpritemap_23_A6DE02:
+dw $0002 : db $DC,$81,$E4,$08,$71, $E4,$81,$E4,$07,$71
+
+ProfaneJunkoTailTipWingSpritemap_24_A6DE1D:
+dw $0002 : db $DC,$01,$F0,$36,$71, $E4,$81,$E8,$24,$71
+
+ProfaneJunkoTailTipWingSpritemap_25_A6DE2E:
+dw $0002 : db $DC,$01,$E8,$36,$F1, $E4,$81,$E8,$24,$F1
+
+ProfaneJunkoTailTipWingSpritemap_26_A6DE3F:
+dw $0002 : db $DC,$81,$EA,$08,$F1, $E4,$81,$EA,$07,$F1
+
+ProfaneJunkoTailTipWingSpritemap_27_A6DE5A:
+dw $0002 : db $DC,$81,$EF,$05,$F1, $E4,$81,$EF,$04,$F1
+
+ProfaneJunkoFireballSpritemap_0_8D80CA:
+dw $0001 : db $F8,$81,$F8,$0A,$31
+
+ProfaneJunkoFireballSpritemap_1_8D80D1:
+dw $0001 : db $F8,$81,$F8,$0C,$F1
+
+ProfaneJunkoFireballSpritemap_2_8D80D8:
+dw $0001 : db $F8,$81,$F8,$0A,$F1
+
+ProfaneJunkoFireballSpritemap_3_8D80DF:
+dw $0001 : db $F8,$81,$F8,$0C,$31
+
+ProfaneJunkoHitbox_0_A6EAE1:
+dw $0002, -12,-26,11,13,$DF59,$DF8A, -24,3,-13,21,$DF59,$DF8A
+
+ProfaneJunkoHitbox_1_A6EAFB:
+dw $0002, -41,-19,-21,-9,$DF59,$DF8A, -20,-29,11,5,$DF59,$DF8A
+
+ProfaneJunkoHitbox_2_A6EB15:
+dw $0002, -37,-40,-14,-31,$DF59,$DF8A, -25,-31,9,6,$DF59,$DF8A
+
+ProfaneJunkoHitbox_3_A6EB2F:
+dw $0001, -15,-10,7,2,$DF59,$DF8A
+
+ProfaneJunkoHitbox_4_A6EB3D:
+dw $0001, -17,-9,6,15,$DF59,$DF8A
+
+ProfaneJunkoHitbox_5_A6EB4B:
+dw $0001, -14,-1,10,23,$DF59,$DF8A
+
+ProfaneJunkoHitbox_6_A6EB59:
+dw $0001, -15,-2,-1,8,$DF59,$DF8A
+
+ProfaneJunkoHitbox_7_A6EB67:
+dw $0001, -16,-20,12,21,$DF59,$DF8A
+
+ProfaneJunkoHitbox_10_A6EB91:
+dw $0002, -16,-32,16,34,$DF59,$DF8A, -8,-45,8,-33,$DF59,$DF8A
+
+ProfaneJunkoHitbox_11_A6EBAB:
+dw $0002, -12,-25,11,13,$DF59,$DF8A, 12,5,24,20,$DF59,$DF8A
+
+ProfaneJunkoHitbox_12_A6EBC5:
+dw $0002, -13,-29,20,5,$DF59,$DF8A, 21,-18,39,-8,$DF59,$DF8A
+
+ProfaneJunkoHitbox_13_A6EBDF:
+dw $0002, -10,-31,25,8,$DF59,$DF8A, 13,-42,35,-32,$DF59,$DF8A
+
+ProfaneJunkoHitbox_14_A6EBF9:
+dw $0001, -10,-10,17,2,$DF59,$DF8A
+
+ProfaneJunkoHitbox_15_A6EC07:
+dw $0001, -9,-8,17,15,$DF59,$DF8A
+
+ProfaneJunkoHitbox_16_A6EC15:
+dw $0001, -11,-8,14,23,$DF59,$DF8A
+
+ProfaneJunkoHitbox_17_A6EC23:
+dw $0001, 1,-2,14,9,$DF59,$DF8A
+
+ProfaneJunkoHitbox_18_A6EC31:
+dw $0001, -13,-22,14,21,$DF59,$DF8A
+
+ProfaneJunkoExtSpritemap_0_A6E983:
+dw $0004, 15,22,ProfaneJunkoSpritemap_3_A6ED29,ProfaneJunkoHitbox_3_A6EB2F, -8,7,ProfaneJunkoSpritemap_6_A6ED8E,ProfaneJunkoHitbox_6_A6EB59, 16,0,ProfaneJunkoSpritemap_7_A6ED95,ProfaneJunkoHitbox_7_A6EB67, -3,-24,ProfaneJunkoSpritemap_0_A6EC5B,ProfaneJunkoHitbox_0_A6EAE1
+
+ProfaneJunkoExtSpritemap_1_A6E9A5:
+dw $0004, -15,22,ProfaneJunkoSpritemap_12_A6EF25,ProfaneJunkoHitbox_14_A6EBF9, 8,7,ProfaneJunkoSpritemap_15_A6EF8A,ProfaneJunkoHitbox_17_A6EC23, -16,0,ProfaneJunkoSpritemap_16_A6EF91,ProfaneJunkoHitbox_18_A6EC31, 3,-24,ProfaneJunkoSpritemap_9_A6EE57,ProfaneJunkoHitbox_11_A6EBAB
+
+ProfaneJunkoExtSpritemap_2_A6E9C7:
+dw $0004, 15,22,ProfaneJunkoSpritemap_3_A6ED29,ProfaneJunkoHitbox_3_A6EB2F, -8,7,ProfaneJunkoSpritemap_6_A6ED8E,ProfaneJunkoHitbox_6_A6EB59, 16,0,ProfaneJunkoSpritemap_7_A6ED95,ProfaneJunkoHitbox_7_A6EB67, -3,-24,ProfaneJunkoSpritemap_1_A6EC99,ProfaneJunkoHitbox_1_A6EAFB
+
+ProfaneJunkoExtSpritemap_3_A6E9E9:
+dw $0004, 15,22,ProfaneJunkoSpritemap_3_A6ED29,ProfaneJunkoHitbox_3_A6EB2F, -8,7,ProfaneJunkoSpritemap_6_A6ED8E,ProfaneJunkoHitbox_6_A6EB59, 16,0,ProfaneJunkoSpritemap_7_A6ED95,ProfaneJunkoHitbox_7_A6EB67, -3,-24,ProfaneJunkoSpritemap_2_A6ECDC,ProfaneJunkoHitbox_2_A6EB15
+
+ProfaneJunkoExtSpritemap_4_A6EA0B:
+dw $0004, -15,22,ProfaneJunkoSpritemap_12_A6EF25,ProfaneJunkoHitbox_14_A6EBF9, 8,7,ProfaneJunkoSpritemap_15_A6EF8A,ProfaneJunkoHitbox_17_A6EC23, -16,0,ProfaneJunkoSpritemap_16_A6EF91,ProfaneJunkoHitbox_18_A6EC31, 3,-24,ProfaneJunkoSpritemap_10_A6EE95,ProfaneJunkoHitbox_12_A6EBC5
+
+ProfaneJunkoExtSpritemap_5_A6EA2D:
+dw $0004, -15,22,ProfaneJunkoSpritemap_12_A6EF25,ProfaneJunkoHitbox_14_A6EBF9, 8,7,ProfaneJunkoSpritemap_15_A6EF8A,ProfaneJunkoHitbox_17_A6EC23, -16,0,ProfaneJunkoSpritemap_16_A6EF91,ProfaneJunkoHitbox_18_A6EC31, 3,-24,ProfaneJunkoSpritemap_11_A6EED8,ProfaneJunkoHitbox_13_A6EBDF
+
+ProfaneJunkoExtSpritemap_6_A6EA4F:
+dw $0004, 15,22,ProfaneJunkoSpritemap_4_A6ED4E,ProfaneJunkoHitbox_4_A6EB3D, -8,7,ProfaneJunkoSpritemap_6_A6ED8E,ProfaneJunkoHitbox_6_A6EB59, 16,0,ProfaneJunkoSpritemap_7_A6ED95,ProfaneJunkoHitbox_7_A6EB67, -3,-24,ProfaneJunkoSpritemap_0_A6EC5B,ProfaneJunkoHitbox_0_A6EAE1
+
+ProfaneJunkoExtSpritemap_7_A6EA71:
+dw $0004, 15,22,ProfaneJunkoSpritemap_5_A6ED6E,ProfaneJunkoHitbox_5_A6EB4B, -8,7,ProfaneJunkoSpritemap_6_A6ED8E,ProfaneJunkoHitbox_6_A6EB59, 16,0,ProfaneJunkoSpritemap_7_A6ED95,ProfaneJunkoHitbox_7_A6EB67, -3,-24,ProfaneJunkoSpritemap_0_A6EC5B,ProfaneJunkoHitbox_0_A6EAE1
+
+ProfaneJunkoExtSpritemap_8_A6EA93:
+dw $0004, -15,22,ProfaneJunkoSpritemap_13_A6EF4A,ProfaneJunkoHitbox_15_A6EC07, 8,7,ProfaneJunkoSpritemap_15_A6EF8A,ProfaneJunkoHitbox_17_A6EC23, -16,0,ProfaneJunkoSpritemap_16_A6EF91,ProfaneJunkoHitbox_18_A6EC31, 3,-24,ProfaneJunkoSpritemap_9_A6EE57,ProfaneJunkoHitbox_11_A6EBAB
+
+ProfaneJunkoExtSpritemap_9_A6EAB5:
+dw $0004, -15,22,ProfaneJunkoSpritemap_14_A6EF6A,ProfaneJunkoHitbox_16_A6EC15, 8,7,ProfaneJunkoSpritemap_15_A6EF8A,ProfaneJunkoHitbox_17_A6EC23, -16,0,ProfaneJunkoSpritemap_16_A6EF91,ProfaneJunkoHitbox_18_A6EC31, 3,-24,ProfaneJunkoSpritemap_9_A6EE57,ProfaneJunkoHitbox_11_A6EBAB
+
+ProfaneJunkoExtSpritemap_10_A6EAD7:
+dw $0001, 0,-6,ProfaneJunkoSpritemap_8_A6EDB5,ProfaneJunkoHitbox_10_A6EB91
